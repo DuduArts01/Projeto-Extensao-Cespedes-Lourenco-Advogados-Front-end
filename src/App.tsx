061e568699a0a2216279f34962b71c13cd87ff9d
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2026-05-12 21:12:55 -0300

    initial front-end configurations

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+      <div className="w-32 h-16 bg-blue-500 flex items-center justify-center rounded-2xl">
+        <p className="text-white font-bold">Hello World</p>
+      </div>
+  )
+}
+
+export default App
